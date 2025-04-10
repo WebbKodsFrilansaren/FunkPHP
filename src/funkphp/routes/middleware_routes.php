7f2a155cee8198d4dcaf50edce63de23c92512e5
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-04-10 22:53:11 +0200

    Few more boilerpalte exampels in MW routes

--- src/funkphp/routes/middleware_routes.php
+++ src/funkphp/routes/middleware_routes.php
@@ -10,4 +10,10 @@ return [
         '/users' => ['handler' => 'POST_MIDDLEWARE_USER', /*...*/],
         '/users/:id' => ['handler' => 'POST_MIDDLEWARE_USER_PROFILE_TEST', /*...*/],
     ],
+    'PUT' => [
+        '/users/:id' => ['handler' => 'PUT_MIDDLEWARE_USER', /*...*/],
+    ],
+    'DELETE' => [
+        '/users/:id' => ['handler' => 'DELETE_MIDDLEWARE_USER', /*...*/],
+    ],
 ];
