15f1924a8dd521b01fcd0c7afb57b85a571e211d
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-04-15 08:06:54 +0200

    CLI Functions in separate file (used by "COMPILE_ROUTES" script!) now!

---
+++ src/funkphp/_internals/functions/cli_funs.php
@@ -0,0 +1,307 @@
+<?php
+
+// Build Compiled Route from Developer's Defined Routes
+function cli_build_compiled_routes(array $developerSingleRoutes, array $developerMiddlewareRoutes)
+{
+    // Only localhost can run this function (meaning you cannot run this in production!)
+    // Both arrays must be non-empty arrays
+    if (!is_array($developerSingleRoutes)) {
+        echo "[ERROR]: '\$developerSingleRoutes' Must be a non-empty array!\n";
+        exit;
+    } elseif (!is_array($developerMiddlewareRoutes)) {
+        echo "[ERROR]: '\$developerMiddlewareRoutes' Must be a non-empty array!\n";
+        exit;
+    }
+    if (empty($developerSingleRoutes)) {
+        echo "[ERROR]: Must '\$developerSingleRoutes' be a non-empty array!\n";
+        exit;
+    } else if (empty($developerMiddlewareRoutes)) {
+        echo "[ERROR]: Must '\$developerMiddlewareRoutes' be a non-empty array!\n";
+        exit;
+    }
+
+    // Prepare compiled route array to return and other variables
+    $compiledTrie = [];
+    $GETSingles = $developerSingleRoutes["GET"] ?? null;
+    $POSTSingles = $developerSingleRoutes["POST"] ?? null;
+    $PUTSingles = $developerSingleRoutes["PUT"] ?? null;
+    $DELETESingles = $developerSingleRoutes["DELETE"] ?? null;
+
+    // Using method below, iterate through each HttpMethod and then add it to the $compiledTrie array
+    $addMethods = function ($singleRoutes) {
+        // Begin with just getting the key names and no other nested values inside of them:
+        // For example:  '/users' => ['handler' => 'USERS_PAGE', /*...*/], only gets the '/users' key name
+        // and not the value inside of it. This is done by using array_keys() to get the keys of the array.
+        $keys = array_keys($singleRoutes) ?? [];
+        $compiledTrie = [];
+
+        // Iterate through each key in the array and add it to the $compiledTrie array
+        foreach ($keys as $key) {
+
+            // Ignore empty keys or null values & handle special case for "/"
+            if ($key === "" || $key === null || $key === false || $key === "") {
+                continue;
+            }
+            if ($key === "/") {
+                $compiledTrie["/"] = [];
+                continue;
+            }
+
+            // Split the route into segments
+            $splitRouteSegments = explode("/", trim($key, "/"));
+
+            // Initialize the current node in the trie
+            $currentNode = &$compiledTrie;
+
+            // Iterate through each segment of the route
+            foreach ($splitRouteSegments as $segment) {
+                // WHEN DYNAMIC PARAMETER ROUTE SEGMENT
+                if (str_starts_with($segment, ":")) {
+                    // Create when not exist
+                    if (!isset($currentNode[':'])) {
+                        $currentNode[':'] = [];
+                    }
+                    // And insert param as next nested key and/or move to next node
+                    $paramName = substr($segment, 1);
+                    if (!isset($currentNode[':'][$paramName])) {
+                        $currentNode[':'][$paramName] = [];
+                    }
+                    $currentNode = &$currentNode[':'][$paramName];
+                }
+                // WHEN LITERAL ROUTE SEGMENT
+                else {
+                    // Insert if not exist and/or move to next node
+                    if (!isset($currentNode[$segment])) {
+                        $currentNode[$segment] = [];
+                    }
+                    $currentNode = &$currentNode[$segment];
+                }
+            }
+        }
+        // Return the compiled trie for the method
+        return $compiledTrie;
+    };
+
+    // Add the middleware routes to the compiled trie
+    $addMiddlewareRoutes = function ($middlewareRoutes, &$compiledTrie) {
+        // Only extract the keys from the middleware routes
+        $keys = array_keys($middlewareRoutes) ?? [];
+
+        // The way we insert "|" to signify a middleware is to just go through all segments for each key
+        // and when we are at the last segment that is the node we insert "|" and then we move on to key.
+        foreach ($keys as $key) {
+            // Ignore empty keys or null values & handle special case for "/"
+            if ($key === "" || $key === null || $key === false || $key === "") {
+                continue;
+            }
+            if ($key === "/") {
+                $compiledTrie["|"] = [];
+                continue;
+            }
+
+            // Now split key into segments and iterate through each segment
+            $splitRouteSegments = explode("/", trim($key, "/"));
+
+            // Now we just navigate to the last segment and add the middleware node "|".
+            // We just check what it is and then just navigate,
+            $currentNode = &$compiledTrie;
+
+            // So we just check one of three things: is there a literal route to navigate to?
+            // is there a dynamic route to navigate to? or is it a middleware node? WE JUST NAVIGATE TO IT
+            // until we run out of segments, that means we have reached the node where we insert the middleware node "|".
+            foreach ($splitRouteSegments as $segment) {
+                // SPECIAL CASE: Navigate past any middleware node "|" but not at root node!
+                if (isset($currentNode['|']) && !empty($currentNode['|'])) {
+                    $currentNode = &$currentNode['|'];
+                }
+
+                // LITERAL ROUTE SEGMENT
+                if (isset($currentNode[$segment])) {
+                    $currentNode = &$currentNode[$segment];
+                    continue;
+                }
+
+                // DYNAMIC ROUTE SEGMENT
+                elseif (str_starts_with($segment, ":")) {
+                    $paramName = substr($segment, 1);
+                    $currentNode = &$currentNode[':'][$paramName];
+                    continue;
+                }
+            }
+
+            // Now we are at the last segment, we just add the middleware node "|"
+            // and then we add the middleware route to it.
+            if (!isset($currentNode['|'])) {
+                $currentNode['|'] = [];
+            }
+        }
+    };
+
+    // First add the single routes to the compiled trie
+    $compiledTrie['GET'] = $addMethods($GETSingles);
+    $compiledTrie['POST'] = $addMethods($POSTSingles);
+    $compiledTrie['PUT'] = $addMethods($PUTSingles);
+    $compiledTrie['DELETE'] = $addMethods($DELETESingles);
+
+    // Then add the middlewares to the compiled trie and return it
+    $addMiddlewareRoutes($developerMiddlewareRoutes["GET"] ?? [], $compiledTrie['GET']);
+    $addMiddlewareRoutes($developerMiddlewareRoutes["POST"] ?? [], $compiledTrie['POST']);
+    $addMiddlewareRoutes($developerMiddlewareRoutes["PUT"] ?? [], $compiledTrie['PUT']);
+    $addMiddlewareRoutes($developerMiddlewareRoutes["DELETE"] ?? [], $compiledTrie['DELETE']);
+    // addMiddlewareRoutes($developerMiddlewareRoutes["GET"] ?? [], $compiledTrie['GET']);
+    // addMiddlewareRoutes($developerMiddlewareRoutes["POST"] ?? [], $compiledTrie['POST']);
+    // addMiddlewareRoutes($developerMiddlewareRoutes["PUT"] ?? [], $compiledTrie['PUT']);
+    // addMiddlewareRoutes($developerMiddlewareRoutes["DELETE"] ?? [], $compiledTrie['DELETE']);
+
+    return $compiledTrie;
+}
+
+// Output Compiled Route to File or Return as String
+function cli_output_compiled_routes(array $compiledTrie, string $outputFileNameFolderIsAlways_compiled_routes = "null")
+{
+    // Check if the compiled route is empty
+    if (!is_array($compiledTrie)) {
+        echo "[ERROR]: Compiled Routes Must Be A Non-Empty Array!\n";
+        exit;
+    }
+    if (empty($compiledTrie)) {
+        echo "[ERROR]: Compiled Routes Must Be A Non-Empty Array!\n";
+        exit;
+    }
+
+    // TODO: Add the audit function check here!
+
+    // Output either to file destiation or in current folder as datetime in file name
+    $datetime = date("Y-m-d_H-i-s");
+    $outputDestination = $outputFileNameFolderIsAlways_compiled_routes === "null" ? dirname(__DIR__) . "/compiled/troute_" . $datetime . ".php" : dirname(__DIR__) . "\/compiled\/" . $outputFileNameFolderIsAlways_compiled_routes . ".php";
+
+    // Check if file already exists
+    if (file_exists($outputDestination)) {
+        echo "[INFO]: FILE EXISTS. THIS OVERWRITES THE FILE!\n";
+    }
+
+    $result = null;
+    if ($outputFileNameFolderIsAlways_compiled_routes !== "null") {
+        $result = file_put_contents(dirname(__DIR__) . "/compiled/" . $outputFileNameFolderIsAlways_compiled_routes . ".php", "<?php\nreturn " . cli_convert_array_to_simple_syntax($compiledTrie));
+    } else {
+        $result = file_put_contents($outputDestination, "<?php\nreturn " . cli_convert_array_to_simple_syntax($compiledTrie));
+    }
+    if ($result === false) {
+        echo "[ERROR]: Compiled routes was NOT written to: $outputDestination\n!";
+    } else {
+        echo "[SUCCESS: Compiled routes written to: $outputDestination\n!";
+    }
+}
+
+// Audit Developer's Defined Routes
+function cli_audit_developer_routes(array $developerSingleRoutes, array $developerMiddlewareRoutes): array
+{
+    // Both arrays must be non-empty arrays
+    if (!is_array($developerSingleRoutes)) {
+        echo "[ERROR]: '\$developerSingleRoutes' Must be a non-empty array!\n";
+        exit;
+    } elseif (!is_array($developerMiddlewareRoutes)) {
+        echo "[ERROR]: '\$developerMiddlewareRoutes' Must be a non-empty array!\n";
+        exit;
+    }
+    if (empty($developerSingleRoutes)) {
+        echo "[ERROR]: Must '\$developerSingleRoutes' be a non-empty array!\n";
+        exit;
+    } else if (empty($developerMiddlewareRoutes)) {
+        echo "[ERROR]: Must '\$developerMiddlewareRoutes' be a non-empty array!\n";
+        exit;
+    }
+
+    // Prepare result variable
+    $auditResult = [];
+
+    return $auditResult;
+}
+
+// Convert PHP array() syntax to simplified [] syntax
+function cli_convert_array_to_simple_syntax(array $array): string | null | array
+{
+    // Must be non-empty array
+    if (!is_array($array)) {
+        return ["err" => "[r_convert_array_to_simple_syntax]: Must be a non-empty array!"];
+    }
+
+    // Check if the array is empty
+    if (empty($array)) {
+        ["err" => "[r_convert_array_to_simple_syntax]: Must be a non-empty array!"];
+    }
+
+    // Prepare array and parse state variables
+    $str = mb_str_split(var_export($array, true));
+    $arrStack = [];
+    $arrayLetters = ["a", "r", "r", "a", "y", " "];
+    $quotes = ["'", '"'];
+    $inStr = false;
+    $converted = "";
+
+    // Check if first character is "a"
+    if ($str[0] !== "a") {
+        return ["err" => "[r_convert_array_to_simple_syntax]: Invalid var_export array syntax! Expected 'array ('"];
+    }
+
+
+    // Parse on each character of the prepared string
+    for ($i = 0; $i < count($str); $i++) {
+        $c = $str[$i];
+
+        // If inside string and is not a quote
+        if ($inStr && (!in_array($c, $quotes) && $c !== "\\")) {
+            $converted .= $c;
+            continue;
+        }
+        // If inside string with escaped character, just skip it
+        elseif ($inStr && ($c === "\\")) {
+            $i++;
+            continue;
+        }
+        // If inside string and is a quote
+        elseif ($inStr && (in_array($c, $quotes))) {
+            $converted .= $c;
+            $inStr = false;
+            continue;
+        }
+
+        // If not inside string and is a quote
+        if (!$inStr && empty($arrStack) && (in_array($c, $quotes))) {
+            $inStr = true;
+            $converted .= $c;
+            continue;
+        }
+
+        // If not inside string and next character is "a" from "array (" & not from false boolean
+        if (!$inStr && empty($arrStack)  && $c === "a" && $str[$i + 1] !== "l") {
+            $arrStack[] = $c;
+            continue;
+        }
+
+        // If not inside string and next character is one from:"rray ("
+        if (!$inStr && !empty($arrStack)) {
+            if (count($arrStack) < 5 && in_array($c, $arrayLetters)) {
+                $arrStack[] = $c;
+                continue;
+
+                // If not inside string and next character is "(" from "array ("
+            } elseif (count($arrStack) === 5 && $c === "(") {
+                $converted .= "[";
+                unset($arrStack);
+                continue;
+            }
+        }
+
+        // If outside string and ")"
+        if (!$inStr && $c === ")") {
+            $converted .= "]";
+            continue;
+        }
+        $converted .= $c;
+    }
+
+    // Return the finalized string varaible
+    $converted .= ";";
+    return $converted;
+}
