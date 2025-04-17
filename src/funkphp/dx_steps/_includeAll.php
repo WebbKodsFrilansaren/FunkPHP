0acf3f06812b05d42ae791d83718cb0bd9582d1c
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-04-17 18:07:35 +0200

    Removed comment

--- src/funkphp/dx_steps/_includeAll.php
+++ src/funkphp/dx_steps/_includeAll.php
@@ -1,5 +1,4 @@
 <?php
-//include_once __DIR__ . '/STEP3_RUN_MIDDLEWARES_AFTER_MATCH.php';
 include_once __DIR__ . '/STEP1_INITIALIZE_GLOBAL_CONFIG.php';
 include_once __DIR__ . '/STEP2_FILTER_METHODS_IPS_UAS.php';
 include_once __DIR__ . '/STEP3_ROUTE_MATCH_MIDDLEWARES.php';
