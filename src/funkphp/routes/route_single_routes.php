c471db4b9d50c0ad843127f3bf9a7af040cfc1b8
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-02 15:36:55 +0200

    demo done

--- src/funkphp/routes/route_single_routes.php
+++ src/funkphp/routes/route_single_routes.php
@@ -1,4 +1,4 @@
-<?php // ROUTE_SINGLE_ROUTES.PHP - FunkPHP Framework | This File Was Modified In FunkCLI 2025-05-02 15:22:14
+<?php // ROUTE_SINGLE_ROUTES.PHP - FunkPHP Framework | This File Was Modified In FunkCLI 2025-05-02 15:35:46
 return  [
     'ROUTES' =>
     [
