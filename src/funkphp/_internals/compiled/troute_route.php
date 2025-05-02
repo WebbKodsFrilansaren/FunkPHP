2443d9dc76aa8f91b86f6ce5d6cc208be969838b
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-02 21:01:25 +0200

    fix

--- src/funkphp/_internals/compiled/troute_route.php
+++ src/funkphp/_internals/compiled/troute_route.php
@@ -1,27 +1,11 @@
 <?php
 return  [
-  'GET' => 
-   [
-    'admin' => 
-     [
-    ],
-    'fagala' => 
-     [
-    ],
-    'fagala2' => 
-     [
-    ],
-    'users' => 
-     [
-    ],
-  ],
-  'POST' => 
-   [
-  ],
-  'PUT' => 
-   [
-  ],
-  'DELETE' => 
-   [
-  ],
+  'GET' =>
+  [],
+  'POST' =>
+  [],
+  'PUT' =>
+  [],
+  'DELETE' =>
+  [],
 ];
