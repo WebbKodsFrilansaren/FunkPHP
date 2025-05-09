f9bc6ab7e2427b3b8d5feb5baace98e71db03d28
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-09 14:31:10 +0200

    fix

--- src/funkphp/sql/test_authors.sql
+++ src/funkphp/sql/test_authors.sql
@@ -5,8 +5,7 @@ CREATE TABLE authors(
     description tinytext NOT NULL DEFAULT "No description",
     longer_description text NOT NULL DEFAULT "No longer description",
     age INT(3) DEFAULT 18 SIGNED,
-    enum_test ENUM('a test', 'b or more', 'c what i did') DEFAULT 'c',
-    set_test SET('every', 'unique', 'carrot or morot') DEFAULT 'a,b',
+    enum_test ENUM('a test', 'b or more', 'c what i did') DEFAULT 'c what i did',
     weight FLOAT DEFAULT 70.0 NOT NULL,
     nickname NVARCHAR(255) DEFAULT "Anonymous",
     updated_at TIME DEFAULT NOW()
