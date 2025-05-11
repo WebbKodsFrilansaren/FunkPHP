71762be92c96edd8fb897c830823e4552977be5f
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-11 17:18:43 +0200

    removed no longer used stuff

--- src/funkphp/_internals/templates/page_template.php
+++ src/funkphp/_internals/templates/page_template.php
@@ -1,37 +1,36 @@
-<?php // TEMPLATE For FunkPHP Rendered Pages (the final step unless you already sent a JSON Response!)
+<!-- // TEMPLATE For FunkPHP Rendered Pages (the final step unless you already sent a JSON Response!)
+    /****** HOW TO USE THIS FILE: ******/
+    // Write your Page using HTML and the typical Template Engine Syntax you are used to!) Make sure
+    // the filename is exactly as in 'page'=> 'page_file_name_to_template_return' in the routes file.
+    //
+    //
+    /****** AVAILABLE TEMPLATE ENGINE SYNTAX ******
+    * - {{variable}} / {{ variable }} = Output the variable value (HTML escaped), outputs "" if null
+    *
+    * - {{variable|"WhenNull"}} / {{ variable|"WhenNull" }} = Output the variable value (HTML escaped) or "WhenNull" if null
+    *
+    * - {!!!!?variable?!!!!} = Output the variable value (HTML NOT escaped) - ONLY USE IF YOU 100 % SANITIZED THE VARAIBLE!!!!
+    *
+    * - @if(condition) = Start of an if statement, remember the @endif at the end of the statement!
+    *
+    * - @elseif(condition) = Else if statement, can be used multiple times in a row; remember the @endif at the end of the statement!
+    *
+    * - @else = Else statement, can be used once and should be used before @endif; remember the @endif at the end of the statement!
+    *
+    * - @endif = End of an if statement, remember to use @if and/or @elseif and/or @else before you use this statement!
+    *
+    * - @foreach($loop_design) = Start of a foreach loop, remember to use @endforeach at the end of the loop!
+    * - For example: @foreach($array as $element) OR @foreach($array as $key => $element)
+    *
+    * - @endforeach = End of a foreach loop, remember to use @foreach before you use this statement!
+    *
+    * - @part(filename) = includes the filename with default DIR at "src/funkphp/pages/parts/"
+    *
+    * - @auth() = Auth check, if the user is logged in, it will show the content inside the @auth and @endauth tags.
+    *
+    * - @endauth = End of the auth check, remember to use @auth before you use this statement!
+    *
+    -->
+<html lang="en">
 
-/****** HOW TO USE THIS FILE: ******/
-// Write your Page using HTML and the typical Template Engine Syntax you are used to!) Make sure
-// the filename is exactly as in 'page' => 'page_file_name_to_template_return' in the routes file.
-//
-//
-/****** AVAILABLE TEMPLATE ENGINE SYNTAX ******
- * - {{$variable}} / {{ $variable }} = Output the variable value (HTML escaped), outputs "" if null
- *
- * - {{$variable|"WhenNull"}} / {{ $variable|"WhenNull" }} = Output the variable value (HTML escaped) or "WhenNull" if null
- *
- * - {!!!!?$variable?!!!!} = Output the variable value (HTML NOT escaped) - ONLY USE IF YOU 100 % SANITIZED THE VARAIBLE!!!!
- *
- * - @if(condition) = Start of an if statement, remember the @endif at the end of the statement!
- *
- * - @elseif(condition) = Else if statement, can be used multiple times in a row; remember the @endif at the end of the statement!
- *
- * - @else = Else statement, can be used once and should be used before @endif; remember the @endif at the end of the statement!
- *
- * - @endif = End of an if statement, remember to use @if and/or @elseif and/or @else before you use this statement!
- *
- * - @foreach($loop_design) = Start of a foreach loop, remember to use @endforeach at the end of the loop!
- *   - For example: @foreach($array as $element) OR @foreach($array as $key => $element)
- *
- * - @endforeach = End of a foreach loop, remember to use @foreach before you use this statement!
- *
- * - @part(filename) = includes the filename with default DIR at "src/funkphp/pages/parts/"
- *
- * - @auth() = Auth check, if the user is logged in, it will show the content inside the @auth and @endauth tags.
- *
- * - @endauth = End of the auth check, remember to use @auth before you use this statement!
- */
-
-// Always include &$c so you can use the processed data,
-// now when you are finally at the end of the Request!
-return function (&$c) {};
+</html>
