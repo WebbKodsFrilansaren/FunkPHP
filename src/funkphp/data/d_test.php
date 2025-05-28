b4716e36c14f88e62004295166a6bca7c0a5696f
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-28 13:14:50 +0200

    just work with GET

--- src/funkphp/data/d_test.php
+++ src/funkphp/data/d_test.php
@@ -7,7 +7,7 @@ function d_test(&$c) // <POST/test/:id>
     // Created in FunkCLI on 2025-05-24 08:52:25! Keep "};" on its
     // own new line without indentation no comment right after it!
     $v_file = funk_use_validation_get_validation_array_or_err_out($c, "v_test");
-    $validatePOST = funk_use_validation($c, $v_file, "JSON");
+    $validatePOST = funk_use_validation($c, $v_file, "GET");
     $test = [
         'v' => $c['v'],
         'v_ok' => $c['v_ok'],
