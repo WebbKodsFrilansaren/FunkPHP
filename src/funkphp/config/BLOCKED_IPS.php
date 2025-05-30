9604118347cbdc0a260599368998fcecf629990f
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-05-30 21:58:58 +0200

    clarification

--- src/funkphp/config/BLOCKED_IPS.php
+++ src/funkphp/config/BLOCKED_IPS.php
@@ -1,5 +1,5 @@
 <?php // GLOBAL BLOCK OF EXACTLY MATCHED IPs
 // This filtering occurs AFTER valid method check and BEFORE UA check!
-// Syntax to add an exactly matched IP: "IP_EXACT_MATCH => []"
+// Syntax to add an exactly matched IP: "1-255.1-255.1-255.1-255 => []"
 
 return [];
