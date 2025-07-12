778d6b13f4c766ea360cd95285cd0fbe748836bb
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-07-13 01:24:18 +0200

    yas

--- src/funkphp/funkphp_start.php
+++ src/funkphp/funkphp_start.php
@@ -1,5 +1,4 @@
 <?php // ENTRY POINT OF EACH HTTP(S) REQUEST thanks to ".htaccess" file
-
 // CHECK DEFAULT FOLDERS & FILES OR CRITICAL ERROR BASED 'ACCEPT' TPYE!
 if (
     !is_readable(__DIR__ . '/config/_all.php')
@@ -7,17 +6,13 @@ if (
 ) {
     critical_err_json_or_html(500);
 }
-
 // Load all functions needed for the
 // FunkPHP Framework Web Application
 include_once __DIR__ . '/_internals/functions/_includeAllExceptCLI.php';
-
 // $c is the global configuration array that
 // will be used throughout the application
 $c = include_once __DIR__ . '/config/_all.php';
-
 $c['ROUTES'] = [];
-
 // When Routes or Trie Compiled Route File(s) not found/non-readable or empty/missing keys!
 if (!file_exists_is_readable_writable(__DIR__ . '/routes/route_single_routes.php')) {
     $c['err']['ROUTES'][] = "Routes in File `funkphp/routes/route_single_routes.php` not found or non-readable!";
@@ -49,7 +44,6 @@ if (
 ) {
     $c['err']['ROUTES'][] = "Routes in File `funkphp/routes/route_single_routes.php` seems empty, please check!";
 }
-
 // Load the Route Configurations or set error if not found!
 if (
     !isset($c['ROUTES']['SINGLES']['<CONFIG>'])
@@ -59,9 +53,7 @@ if (
 } else {
     $c['r_config'] = $c['ROUTES']['SINGLES']['<CONFIG>'];
 }
-
-// BEFORE STEP 1: Do anything you want here before matching the route and middlewares!
-// Here configured & existing middlewares are loaded and runs before route matching!
+// MAIN STEP: Run the Pipeline of Anonymous Functions that control the flow of the request!
 if (
     isset($c['r_config']['pipeline']) &&
     is_array($c['r_config']['pipeline']) &&
@@ -71,32 +63,17 @@ if (
 } else {
     $c['err']['MAYBE']['CONFIG'][] = "No Configured Route Middlewares (`'<CONFIG>' => 'middlewares_before_route_match'`) to load and run before Route Matching. If you expected Middlewares to run before Route Matching, check the `<CONFIG>` key in the Route `funk/routes/route_single_routes.php` File!";
 }
-
-vd($c['err']);
-
-// STEP 3: Return a matched page after route and data matching!
-// Only run this step if the current step is 3
-if ($c['req']['current_step'] === 3) {
-    // This is the last step of the request, so we can run this step now!
-
-    // TODO: Add matching page step. Add running middleware step. Add the Template Engine function too!
-
-    // This is the end of Step 5, you can freely add any final code here!
-    // You have all global (meta) data in $c variable, so you can use it as you please!
-
-    // AFTER STEP 3: Do anything you want here after returning a page unless JSON was
-    // returned insterad. Here the final middlewares are run after everything else is done!
-    if (
-        isset($c['r_config']['middlewares_after_handled_request']) &&
-        is_array($c['r_config']['middlewares_after_handled_request']) &&
-        !empty($c['r_config']['middlewares_after_handled_request'])
-    ) {
-        funk_run_middleware_after_handled_request($c);
-    } else {
-        $c['err']['MAYBE']['PAGES'][] = "No Configured After Request Middlewares (`'<CONFIG>' => 'middlewares_after_handled_request'`) to load and run after handled request. If you expected Middlewares to run After Handled Request, check the `<CONFIG>` key in the Route `funk/routes/route_single_routes.php` File!";
-    }
-}
-// This is the end of the entire request process!
-
-// This part is only executed if the request was not properly handled by the pipeline!
-// Feel free to add your own error handling here and/or easter egg!
+// TODO: Add matching page step. Add running middleware step. Add the Template Engine function too!
+// This is the end of Step 5, you can freely add any final code here!
+// You have all global (meta) data in $c variable, so you can use it as you please!
+// AFTER STEP 3: Do anything you want here after returning a page unless JSON was
+// returned insterad. Here the final middlewares are run after everything else is done!
+// if (
+//     isset($c['r_config']['middlewares_after_handled_request']) &&
+//     is_array($c['r_config']['middlewares_after_handled_request']) &&
+//     !empty($c['r_config']['middlewares_after_handled_request'])
+// ) {
+//     funk_run_middleware_after_handled_request($c);
+// } else {
+//     $c['err']['MAYBE']['PAGES'][] = "No Configured After Request Middlewares (`'<CONFIG>' => 'middlewares_after_handled_request'`) to load and run after handled request. If you expected Middlewares to run After Handled Request, check the `<CONFIG>` key in the Route `funk/routes/route_single_routes.php` File!";
+// }
--- src/funkphp/routes/route_single_routes.php
+++ src/funkphp/routes/route_single_routes.php
@@ -19,6 +19,7 @@ return  [
       12 => 'm_run_matched_route_middlewares',
       13 => 'm_run_matched_route_handler',
       14 => 'm_run_matched_data_handler',
+      15 => '<LAST_STEP>',
     ],
     'middlewares_after_request' =>
     [],
