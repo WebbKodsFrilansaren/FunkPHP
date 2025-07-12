c6921673ca46e87e8eb823f28578a29c491f41c4
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-07-13 00:38:30 +0200

    yas

--- src/funkphp/_internals/functions/r_route_funs.php
+++ src/funkphp/_internals/functions/r_route_funs.php
@@ -95,9 +95,9 @@ function funk_run_middleware_after_matched_routing(&$c)
 
             // Only run middleware if dir, file and callable,
             // then run it and increment the number of ran middlewares
-            $mwDir = dirname(dirname(__DIR__)) . '/middlewares/after_route_match/';
+            $mwDir = dirname(dirname(__DIR__)) . '/middlewares/';
             $mwToRun = $mwDir . $current_mw . '.php';
-            if (is_dir($mwDir) && file_exists($mwToRun)) {
+            if (file_exists($mwToRun)) {
                 $RunMW = include $mwToRun;
                 if (is_callable($RunMW)) {
                     $c['req']['current_middleware_running'] = $current_mw;
