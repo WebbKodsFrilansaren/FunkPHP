6bf45e72c9b704ae8ff9e1f8faf29e0740011b64
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-09-09 06:35:50 +0200

    yas

---
+++ src/funkphp/middlewares/m_test_v1.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FunkPHP\Middlewares\m_test_v1;
+// FunkCLI Created on 2025-09-09 06:35:24!
+
+return function (&$c, $passedValue = null) {
+	// Placeholder Comment so Regex works - Remove & Add Real Code!
+};
