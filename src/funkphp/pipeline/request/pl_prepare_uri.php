f999b5285736f26a05186643642aa0290dab9a54
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2025-10-13 10:02:06 +0200

    yas

--- src/funkphp/pipeline/request/pl_prepare_uri.php
+++ src/funkphp/pipeline/request/pl_prepare_uri.php
@@ -1,5 +1,11 @@
 <?php
 return function (&$c, $passedValue = null) {
+    // $passedValues i not supported for this pipeline function as of yet
+    if (isset($passedValue)) {
+        $err = 'Tell The Developer - The Pipeline Function `pl_prepare_uri` does not support passed values ($passedValue) as of yet. Please change it back to: `{INT} => ["pl_prepare_uri" => null]` in the Pipeline Configuration.';
+        funk_use_custom_error($c, ['json_or_page', ['json' => ["custom_error" => $err], 'page' => '500'], $err], 500);
+    }
+
     $uri = $_SERVER['REQUEST_URI'];
     $fphp_BASEURL_URI = $c['BASEURLS']['BASEURL_URI'] ?? '/';
     $uri = str_starts_with($_SERVER['REQUEST_URI'], $fphp_BASEURL_URI) ? "/" . ltrim(substr(strtok($_SERVER['REQUEST_URI'], "?"), strlen($fphp_BASEURL_URI)), '/') : strtok($_SERVER['REQUEST_URI'], "?");
