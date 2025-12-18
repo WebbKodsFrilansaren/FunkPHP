dc2d207062a49230cf57d8e0053159c5e05086e5
Max Karlstedt <karlstedt.max@protonmail.com>
2025-12-18 10:26:35 +0100

    yes

--- src/funkphp/routes/routes.php
+++ src/funkphp/routes/routes.php
@@ -1,44 +1,51 @@
-<?php // Routes.php - FunkPHP Framework | FunkCLI Modified it 2025-11-03 14:23:00
+<?php // Routes.php - FunkPHP Framework | FunkCLI Modified it 2025-12-18 09:25:28
 return  [
-  'ROUTES' =>
-  [
-    'GET' =>
-    [
-      '/users/:id' =>
-      [
-        0 =>
-        [
-          'middlewares' =>
-          [
-            0 =>
-            [
+  'ROUTES' => 
+   [
+    'GET' => 
+     [
+      '/users' => 
+       [
+      ],
+      '/users/:id' => 
+       [
+        0 => 
+         [
+          'middlewares' => 
+           [
+            0 => 
+             [
               'mw_test3' => NULL,
             ],
-            1 =>
-            [
+            1 => 
+             [
               'mw_test3' => NULL,
             ],
           ],
         ],
-        1 =>
-        [
-          'try' =>
-          [
-            'test' =>
-            [
+        1 => 
+         [
+          'try' => 
+           [
+            'test' => 
+             [
               'test2' => NULL,
             ],
           ],
         ],
       ],
     ],
-    'DELETE' =>
-    [],
-    'PATCH' =>
-    [],
-    'PUT' =>
-    [],
-    'POST' =>
-    [],
+    'DELETE' => 
+     [
+    ],
+    'PATCH' => 
+     [
+    ],
+    'PUT' => 
+     [
+    ],
+    'POST' => 
+     [
+    ],
   ],
 ];
