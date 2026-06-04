ef9beb2374d0017fdfc7284b76473d12f6f9e3dd
WebbKodsFrilansaren <webbkodslarlingen@gmail.com>
2026-06-04 10:35:30 +0200

    DID CB & GH BOTH GET THIS NOW???

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/funphp/**/*.{php,html}", "./src/gui/**/*.{php,html}"],
+  content: ["./src/funphp/pages/*.{php,html}", "./src/gui/**/*.{php,html}"],
   theme: {
     extend: {},
   },
