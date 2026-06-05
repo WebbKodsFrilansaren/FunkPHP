c49fe1ad255af0d0f00678f545b64ab4e3c2cd10
WebbKodsFrilansaren <webbkodslarlingen@gmail.com>
2026-06-05 09:07:59 +0200

    TWW Scripting & htaccess fixed!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/funphp/pages/*.{php,html}", "./src/gui/**/*.{php,html}"],
+  content: [
+    "./src/funkphp/pages/**/*.{php,html}",
+    "./src/gui/**/*.{php,html}"
+  ],
   theme: {
     extend: {},
   },
